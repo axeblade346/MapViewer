5b837cd4539cee4e4e5381814908bb327e983729
Per Löwgren <per.lowgren@gmail.com>
2020-07-05 17:08:01 +0200

    Changed the method for loading configurations and loading a properties-file instead of config. Added fields serverName, outputFormat for index-file.

--- src/net/spirangle/mapviewer/Config.java
+++ src/net/spirangle/mapviewer/Config.java
@@ -1,29 +1,92 @@
 package net.spirangle.mapviewer;
 
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 public final class Config {
-    private final Path mapDirectory;
-    private final Path originalMapDirectory;
-    private final Path outputDirectory;
-    private final int serverId;
-    private final boolean showDeeds;
-    private final boolean showDeedBorders3d;
-    private final boolean showDeedBordersFlat;
+    private static final Logger logger = Logger.getLogger(Config.class.getName());
 
-    private Config(final Path mapDirectory,final Path originalMapDirectory,final Path outputDirectory,final int serverId,
-                   final boolean showDeeds,final boolean showDeedBorders3d,final boolean showDeedBordersFlat) {
-        this.mapDirectory = mapDirectory;
-        this.originalMapDirectory = originalMapDirectory;
-        this.outputDirectory = outputDirectory;
-        this.serverId = serverId;
-        this.showDeeds = showDeeds;
-        this.showDeedBorders3d = showDeedBorders3d;
-        this.showDeedBordersFlat = showDeedBordersFlat;
+    public static Config instance = null;
+
+    public static Config getInstance() {
+        if(instance==null) instance = new Config();
+        return instance;
+    }
+
+    private enum OutputFormat {
+        HTML,
+        PHP
+    }
+
+    private String serverName;
+    private int serverId;
+    private Path mapDirectory;
+    private Path originalMapDirectory;
+    private Path outputDirectory;
+    private OutputFormat outputFormat;
+    private boolean showDeeds;
+    private boolean showDeedBorders3d;
+    private boolean showDeedBordersFlat;
+
+    private Config() {
+
+    }
+
+    public void load(final Path path,String suffix) throws IOException {
+        if(!Files.exists(path)) return;
+        try(InputStream stream = Files.newInputStream(path)) {
+            Properties properties = new Properties();
+            properties.load(stream);
+            serverName = getProperty(properties,"server-name",suffix);
+            logger.info("Server Name: "+serverName);
+            serverId = Integer.parseInt(getProperty(properties,"server-id",suffix));
+            logger.info("Server ID: "+serverId);
+            mapDirectory = Paths.get(getProperty(properties,"map-directory",suffix),new String[0]);
+            logger.info("Map directory: "+mapDirectory);
+            originalMapDirectory = Paths.get(getProperty(properties,"original-map-directory",suffix),new String[0]);
+            logger.info("Map directory (original): "+originalMapDirectory);
+            outputDirectory = Paths.get(getProperty(properties,"output-directory",suffix),new String[0]);
+            logger.info("Output directory: "+outputDirectory);
+            String of = getProperty(properties,"output-format",suffix);
+            outputFormat = OutputFormat.HTML;
+            if(of!=null) {
+                switch(of.toUpperCase()) {
+                    case "HTML":
+                        outputFormat = OutputFormat.HTML;
+                        break;
+                    case "PHP":
+                        outputFormat = OutputFormat.PHP;
+                        break;
+                }
+            }
+            logger.info("Output Format: "+outputFormat);
+            showDeeds = Boolean.parseBoolean(getProperty(properties,"show-deeds",suffix));
+            logger.info("Show deeds: "+showDeeds);
+            showDeedBorders3d = Boolean.parseBoolean(getProperty(properties,"show-deed-borders-in-3d-mode",suffix));
+            logger.info("Show deed borders (3D): "+showDeedBorders3d);
+            showDeedBordersFlat = Boolean.parseBoolean(getProperty(properties,"show-deed-borders-in-flat-mode",suffix));
+            logger.info("Show deed borders (flat): "+showDeedBordersFlat);
+        }
+    }
+
+    private String getProperty(Properties properties,String key,String suffix) {
+        String value = properties.getProperty(key+suffix);
+        if((value==null || value.isEmpty()) && suffix!=null && !suffix.isEmpty())
+            value = properties.getProperty(key);
+        return value;
+    }
+
+    public String getServerName() {
+        return this.serverName;
+    }
+
+    public int getServerId() {
+        return this.serverId;
     }
 
     public Path getMapDirectory() {
@@ -38,8 +101,12 @@ public final class Config {
         return this.outputDirectory;
     }
 
-    public int getServerId() {
-        return this.serverId;
+    public boolean useHtmlOutput() {
+        return OutputFormat.HTML.equals(this.outputFormat);
+    }
+
+    public boolean usePhpOutput() {
+        return OutputFormat.PHP.equals(this.outputFormat);
     }
 
     public boolean showDeeds() {
@@ -52,57 +119,5 @@ public final class Config {
 
     public boolean showDeedBordersFlat() {
         return this.showDeedBordersFlat;
-    }
-
-    public static Config load(final Path path) throws IOException {
-        try(final BufferedReader reader = Files.newBufferedReader(path)) {
-            Path mapDirectory = null;
-            Path originalMapDirectory = null;
-            Path outputDirectory = null;
-            int serverId = 0;
-            boolean showDeeds = true;
-            boolean showDeedBorders3d = false;
-            boolean showDeedBordersFlat = true;
-            for(String line = reader.readLine(); line!=null; line = reader.readLine()) {
-                if(!line.startsWith("#")) {
-                    final String[] parts = line.split("\\s+=\\s+",2);
-                    if(parts.length==2) {
-                        final String s2;
-                        final String s = s2 = parts[0];
-                        switch(s2) {
-                            case "MAP_DIRECTORY": {
-                                mapDirectory = Paths.get(parts[1],new String[0]);
-                                break;
-                            }
-                            case "ORIGINAL_MAP_DIRECTORY": {
-                                originalMapDirectory = Paths.get(parts[1],new String[0]);
-                                break;
-                            }
-                            case "SHOW_DEED_BORDERS_IN_3D_MODE": {
-                                showDeedBorders3d = Boolean.parseBoolean(parts[1]);
-                                break;
-                            }
-                            case "OUTPUT_DIRECTORY": {
-                                outputDirectory = Paths.get(parts[1],new String[0]);
-                                break;
-                            }
-                            case "SERVER_ID": {
-                                serverId = Integer.parseInt(parts[1]);
-                                break;
-                            }
-                            case "SHOW_DEEDS": {
-                                showDeeds = Boolean.parseBoolean(parts[1]);
-                                break;
-                            }
-                            case "SHOW_DEED_BORDERS_IN_FLAT_MODE": {
-                                showDeedBordersFlat = Boolean.parseBoolean(parts[1]);
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-            return new Config(mapDirectory,originalMapDirectory,outputDirectory,serverId,showDeeds,showDeedBorders3d,showDeedBordersFlat);
-        }
     }
 }
